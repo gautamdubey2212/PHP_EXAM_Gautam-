68e4be7c335934be9973c5521050cd4bd89f9f66
gautamdubey2212 <gautamdubey221203@gmail.com>
2026-04-13 12:02:42 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,16 @@
+MariaDB [php_test]> CREATE TABLE Dashboard (
+    ->     BookId INT AUTO_INCREMENT PRIMARY KEY,
+    ->     BookTitle VARCHAR(50),
+    ->     AuthorName VARCHAR(50),
+    ->     Genre VARCHAR(50),
+    ->     AvailableCopies INT
+    -> );
+Query OK, 0 rows affected (0.029 sec);
+
+create table(
+    fname varchar (50),
+    Username varchar(50),
+    EAddress varchar(50),
+    Phone int (10),
+    Password varchar(255)
+);
